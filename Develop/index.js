438785979f4c67abfa1c1aa12a261855608782c0
Keshia-Lee Martin <keshialeemartin@gmail.com>
2021-10-19 16:00:11 -0400

    added inquirer and first question

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,31 @@
 // TODO: Include packages needed for this application
+const inquirer = require("inquirer");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = () => {
+  console.log(`
+  Professional README Generator
+  =============================
+  `)
+
+  return inquirer.prompt([
+    {
+      type: "input",
+      name: "project",
+      message: "What is the name of your project? (Required)",
+      validate: projectInput => {
+        if (projectInput) {
+          return true;
+        } else {
+          console.log("Please enter your project name!");
+          return false;
+        }
+      }
+    }
+  ])
+};
+
+questions();
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
