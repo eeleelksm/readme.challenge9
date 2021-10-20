b0d5fa6fb1fca8c65b1eacbb040fdf379b15a0f6
Keshia-Lee Martin <keshialeemartin@gmail.com>
2021-10-20 13:54:48 -0400

    readme template

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -14,8 +14,39 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `
   # ${data.title}
-  
 
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents
+
+  * [Installation](#installation)
+  * [Usage](#usage)
+  * [License](#license)
+  * [Contributing](#credits)
+  * [Tests](#tests)
+  * [Questions](#questions)
+  
+  ## Installation
+
+  ${data.install}
+
+  ## Usage
+
+  ${data.instruct}
+
+  ## License
+
+  ## Contributing
+
+  ## Tests
+
+  ## Questions
+
+  If you have any further questions, please contact me at the following:
+  GitHub: [GitHub Profile](https://www.github.com/${data.github}/)
+  Email Address: [${data.email}](mailto:${data.email})
 `;
 }
 
